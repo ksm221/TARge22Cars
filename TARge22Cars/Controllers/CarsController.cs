041a8b902721bc6a491fc83f0658239f0b04737e
Karl <karl.mellikov@gmail.com>
2024-01-08 03:25:09 +0200

    initial setup, layout, settings, etc

---
+++ TARge22Cars/Controllers/CarsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TARge22Cars.Controllers
+{
+    public class CarsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
